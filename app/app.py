fdd4e3bed917878baf699b73eefd1c6e462dbd8b
Juan Bedoya <juan.gonzalez-bed@uniminuto.edu.co>
2026-03-23 01:35:38 +0000

    "Cambios"

--- app/app.py
+++ app/app.py
@@ -6,11 +6,6 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
-try:
-    from flask_debugtoolbar import DebugToolbarExtension
-except ImportError:
-    DebugToolbarExtension = None
-
 from routes.rutas_autenticacion import bp_autenticacion
 from routes.rutas_cliente import bp_cliente
 from routes.rutas_admin import bp_admin
@@ -18,17 +13,12 @@ from routes.rutas_api import bp_api
 
 
 def crear_app():
-    """Fábrica de la aplicación Flask."""
     app = Flask(__name__)
     CORS(app)
-
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'trespasos_secret_2026')
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-    if DebugToolbarExtension:
-        DebugToolbarExtension(app)
 
-    # Registrar blueprints
     app.register_blueprint(bp_autenticacion)
     app.register_blueprint(bp_cliente)
     app.register_blueprint(bp_admin)
